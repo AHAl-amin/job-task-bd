b9cda24d6cbf02add278b62685a24e7e3c5c2902
Alamin573190 <mdalaminhossain573190@gmail.com>
2025-03-08 09:54:08 +0600

    added react environment setup

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -2,7 +2,24 @@
 const Community = () => {
   return (
     <div>
-     <h1>hello community</h1>
+     <h1>rect setup</h1>
+
+     {/* npm create vite@latest my-project -- --template react */}
+     
+     {/* cd my-project */}
+     {/* npm install -D tailwindcss@3 postcss autoprefixer */}
+{/* npx tailwindcss init -p */}
+
+    {/* content bosate hobe */}
+
+    {/* index.css file ta bosate hobe */}
+
+    {/* daisy ui install korte hobe */}
+    {/* daisy ui ar plugins bosate hobe */}
+
+    {/* require ar red color fix korar jonno esling.confige. file "node:true," bosate hoboe */}
+
+
     </div>
   );
 };
